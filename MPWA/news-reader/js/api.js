e51c810a4ffefe7a38d460b09df0463f2c6a0134
amukancabbage <amukancabbage@gmail.com>
2020-05-31 17:12:45 +0800

    PWA begemetean

--- MPWA/news-reader/js/api.js
+++ MPWA/news-reader/js/api.js
@@ -1,4 +1,5 @@
 var base_url = "https://readerapi.codepolitan.com/";
+
 // Blok kode yang akan di panggil jika fetch berhasil
 function status(response) {
     if (response.status !== 200) {
@@ -10,26 +11,27 @@ function status(response) {
         return Promise.resolve(response);
     }
 }
+
 // Blok kode untuk memparsing json menjadi array JavaScript
 function json(response) {
     return response.json();
 }
+
 // Blok kode untuk meng-handle kesalahan di blok catch
 function error(error) {
     // Parameter error berasal dari Promise.reject()
     console.log("Error : " + error);
 }
+
 // Blok kode untuk melakukan request data json
 function getArticles() {
-    fetch(base_url + "articles")
-        .then(status)
-        .then(json)
-        .then(function (data) {
-            // Objek/array JavaScript dari response.json() masuk lewat data.
-            // Menyusun komponen card artikel secara dinamis
-            var articlesHTML = "";
-            data.result.forEach(function (article) {
-                articlesHTML += `
+    if ("caches" in window) {
+        caches.match(base_url + "articles").then(function (response) {
+            if (response) {
+                response.json().then(function (data) {
+                    var articlesHTML = "";
+                    data.result.forEach(function (article) {
+                        articlesHTML += `
                   <div class="card">
                     <a href="./article.html?id=${article.id}">
                       <div class="card-image waves-effect waves-block waves-light">
@@ -42,9 +44,89 @@ function getArticles() {
                     </div>
                   </div>
                 `;
+                    });
+                    // Sisipkan komponen card ke dalam elemen dengan id #content
+                    document.getElementById("articles").innerHTML = articlesHTML;
+                });
+            }
+        });
+    }
+
+    fetch(base_url + "articles")
+        .then(status)
+        .then(json)
+        .then(function (data) {
+            // Objek/array JavaScript dari response.json() masuk lewat data.
+
+            // Menyusun komponen card artikel secara dinamis
+            var articlesHTML = "";
+            data.result.forEach(function (article) {
+                articlesHTML += `
+              <div class="card">
+                <a href="./article.html?id=${article.id}">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${article.thumbnail}" />
+                  </div>
+                </a>
+                <div class="card-content">
+                  <span class="card-title truncate">${article.title}</span>
+                  <p>${article.description}</p>
+                </div>
+              </div>
+            `;
             });
             // Sisipkan komponen card ke dalam elemen dengan id #content
             document.getElementById("articles").innerHTML = articlesHTML;
         })
         .catch(error);
+}
+
+function getArticleById() {
+    // Ambil nilai query parameter (?id=)
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
+
+    if ("caches" in window) {
+        caches.match(base_url + "article/" + idParam).then(function (response) {
+            if (response) {
+                response.json().then(function (data) {
+                    var articleHTML = `
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img src="${data.result.cover}" />
+              </div>
+              <div class="card-content">
+                <span class="card-title">${data.result.post_title}</span>
+                ${snarkdown(data.result.post_content)}
+              </div>
+            </div>
+          `;
+                    // Sisipkan komponen card ke dalam elemen dengan id #content
+                    document.getElementById("body-content").innerHTML = articleHTML;
+                });
+            }
+        });
+    }
+
+    fetch(base_url + "article/" + idParam)
+        .then(status)
+        .then(json)
+        .then(function (data) {
+            // Objek JavaScript dari response.json() masuk lewat variabel data.
+            console.log(data);
+            // Menyusun komponen card artikel secara dinamis
+            var articleHTML = `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="${data.result.cover}" />
+            </div>
+            <div class="card-content">
+              <span class="card-title">${data.result.post_title}</span>
+              ${snarkdown(data.result.post_content)}
+            </div>
+          </div>
+        `;
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("body-content").innerHTML = articleHTML;
+        });
 }
